4a532e04ccec0d0d8e5c2b6fbc1cebd3726b3882
Justice Eshun <je20abe@herts.ac.uk>
2024-02-17 18:17:19 +0000

    Created a loginGui class which allow user to login or launch the register GUI

---
+++ src/GUIS/loginGui.java
@@ -0,0 +1,15 @@
+package GUIS;
+
+/*
+    This gui will allow user to log in or launch the register GUI
+ */
+public class loginGui extends Baseframe {
+    public loginGui() {
+        super("JK Restaurant");
+    }
+
+    @Override
+    protected void addGUIComponents() {
+
+    }
+}
